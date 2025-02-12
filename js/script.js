9ec87d8c47ca05b89772e3e4d4514f546bd4fe88
fotocopyabadi <abadifotocopy01@gmail.com>
2025-02-12 10:56:51 +0700

    fitur baru

--- js/script.js
+++ js/script.js
@@ -7,14 +7,23 @@ document.querySelector("#hamburger-menu").onclick = (e) => {
   e.preventDefault();
 };
 
-
 // Klik diluar elemen
 const hm = document.querySelector("#hamburger-menu");
-
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
-  
+});
+
+// klik pencarian 
+$(document).ready(function(){
+  $("#myinput").on("keyup", function(){
+    var value= $(this).val().toLowerCase();
+
+    $(".container div").filter(function () {
+
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
 });
